6b3490803f302f82742cf76db5752b773eee68da
parkJungWoong <myuoong@gmail.com>
2019-06-23 23:18:13 +0900

    예제 뷰 생성

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # vue-http-client
-vue http 요청 템플릿
+axios를 패키징하여 서비스 로직에서 좀더 사용하기 편하도록 만들어봤습니다.
+특히 jwt 토큰을 이용하는 서비스에 대응할 수 있도록 만들어졌습니다.
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="post()">post request</button>
+    <button v-on:click="get()">get request</button>
+    <button v-on:click="post(true)">post with jwt request</button>
+    <button v-on:click="get(true)">get with jwt request</button>
+    <p>response</p>
+    <textarea class="result" v-model="response" rows="10" disabled></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      response: '{click button}'
+    }
+  },
+  methods: {
+    post(isJwt) {
+      console.log(`post - ${isJwt}`);
+    },
+    get(isJwt) {
+      console.log(`get - ${isJwt}`);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.result {
+  width: 500px;
+}
+</style>
